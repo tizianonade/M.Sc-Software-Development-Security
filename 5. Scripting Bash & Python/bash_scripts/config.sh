ae28405157cb9b68dfc4898f0bca46c448a186c9
tizianoadv <tiziano.nardone.advance@gmail.com>
2022-04-08 14:58:27 +0000

    Script of configuration : Task 4 completed with only the IP Method but not by modifying the yaml file

--- 5. Scripting Bash & Python/bash_scripts/config.sh
+++ 5. Scripting Bash & Python/bash_scripts/config.sh
@@ -15,17 +15,19 @@
 # Arguments received:
 #   $1: Hostname
 #   $2: Id of the network card to configure
-#   $3: Address of the network card 
+#   $3: Address of the network card with netmask
 #   $4: Address of the gateway
 #   $5: Address of DNS
 
 #Function task 1
 change_hostname(){
+    #$1 new hostname
     hostnamectl set-hostname $1
 }
 
 #Function task 2
 check_interface_presence(){
+    #$1 name of interface
 
     #Initialization
     interface_presence=0
@@ -61,13 +63,67 @@ check_interface_presence(){
 }
 
 deactivate_card(){
+    #$1 name of interface
+
     deactivate=$(ip link set $1 down)
     $deactivate
     echo "Interface $1 is deactivated"
 }
 
-# Main
-if [ $# -eq 2 ]
+modify_address(){
+    # Replace string within a file 
+    # Source : https://linuxhint.com/replace_string_in_file_bash/
+    # sed -e "<line number>s/<search pattern>/<replacement string>/"
+    
+    #$1 name of interface
+    #$2 new address of the interface
+
+    #Initialization
+    #i=0
+    #yamlFile="/etc/netplan/00-installer-config.yaml"
+
+    # Get netmask
+    echo "Interface: $1"
+    echo "New address: $2"
+    printf "Type the netmask: "
+    read netmask
+
+    # *** Part 1: Locate the line of the interface within the yaml file ***
+    #catYamlFile=$(cat /etc/netplan/00-installer-config.yaml | awk '{print $1}')
+    
+    #for elmt in $catYamlFile
+    #do
+    #    #Print out interfaces name without the last character ":" 
+    #    interface=${elmt:0:-1}
+
+    #    if [ "$1" = "$interface" ]
+    #    then
+    #        line_interface=$i
+    #    fi
+
+    #   let i++
+    #done
+
+    # *** Part 2: Change dhcp from true to false ***
+    #line_dhcp=$line_interface+1
+    
+    #sed -i "s/      dhcp4: false/      dhcp4: true/" net.yaml
+
+    #echo "      addresses: [10.0.2.17/24]" >> $yamlFile
+    #cat $yamlFile
+   # sed -n '2{h; d}; 4{p; x;}; p' $yamlFile
+
+
+    # IP METHOD
+    setUpNewIp=$(ip addr add $2/$netmask dev $1)
+    #delOldIp=$(ip addr del $OldIp/netmask dev $1)
+
+    $setUpNewIp
+
+}
+
+# Main function
+if [ $# -eq 3 ]
 then
     #Task 1:
     change_hostname $1
@@ -76,7 +132,10 @@ then
     check_interface_presence $2
 
     #Task 3:
-    deactivate_card $2
+    #deactivate_card $2
+
+    #Task 4:
+    modify_address $2 $3
     
 else
     echo "5 arguments required"
